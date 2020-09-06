b13ba7fcf9f9e404235c0f85ea703c849b0b70fa
Simen Husøy <simen@birdsview.no>
2020-09-06 22:16:50 +0200

    stability problems in calculating simple string with mass wave equation

--- src/simple_wave_sim.jl
+++ src/simple_wave_sim.jl
@@ -1,58 +1,36 @@
-# Have to discretize the problem
-#This script is to try to simulate the one dimensional wave equation
-##
+module Simple_wave_sim
 
 push!(LOAD_PATH, "./src/simutils/")
 using DiffEqOperators
 using DifferentialEquations
 using Plots
 using Interact
+plotly()
 
 using Simutils
-
-using Mux, WebIO
-
-#plotlyjs()
 
 ω = 1
 k = 1
 c = (ω/k)^2
-
 number_of_spatial_cells = 100
 dx = 1/(number_of_spatial_cells+1)
-ord_time_deriv = 2
 ord_spacial_deriv = 2
-number_of_time_cells = 100
-dt = 1/(number_of_time_cells + 1)
 
-## Initial conditions
-u_0 = sin.(range(0, 2*pi, length=number_of_spatial_cells))
+# Initial conditions
+u_0 = 0.2.*sin.(range(0, 2*pi, length=number_of_spatial_cells))
 u_dot_0 = zeros(number_of_spatial_cells)
 
+# Defining diff.eq
 A_x = CenteredDifference{1}(2, 2, dx, number_of_spatial_cells)
 Q = Dirichlet0BC(Float64)
-
 u_dot(du, u, p, t) = c*A_x*Q*u
 
 ## Simulate
 prob = SecondOrderODEProblem(u_dot, u_dot_0, u_0, (0.0, 1.0))
 sol = solve(prob, Tsit5())
 
-
-##
-
-mp = @manipulate for t=1:length(sol.u)
-    plot(sol.u[t])
+## Showing results
+du = plot(sol[1:100, 1:10:end])
+u = plot(sol[101:end, 1:10:end])
+display(plot(u, du, layout = (2,1)))
 end
-
-ui = dom"div"(mp)
-webio_serve(page("/", req -> ui), 8000)
-
-# Boundary conditions
-# Initial state
-# simulere
-# Plotte
-# Array med forskjellig karakterestikk
-
-# Issues
-# Why is vector 200 samples long
