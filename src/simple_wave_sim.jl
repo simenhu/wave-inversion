0e1d1a8e9a5f447b2699d1d999d10978424667be
Simen Husøy <simen@birdsview.no>
2020-10-21 15:34:57 +0200

    fixed simple summation in exitation to make soft signal source

--- src/simple_wave_sim.jl
+++ src/simple_wave_sim.jl
@@ -5,6 +5,8 @@ using DifferentialEquations
 using OrdinaryDiffEq
 using LinearAlgebra
 using Plots
+using DataInterpolations
+
 plotlyjs()
 
 using Simutils
@@ -18,20 +20,24 @@ T = 100.0 # N
 c = T/μ
 d = 0.0
 
-# Defining constants for time property
-sim_time = (0.0, 1.1)
-Δt = 0.01
-t = sim_time[1]:Δt:sim_time[2]
-frequency = 50
-u = sin.(2*pi*frequency*t)
-
 number_of_spatial_cells = 100
 dx = (2*pi)/(number_of_spatial_cells+1)
 
 # Initial conditions
-x_0 = 2*sin.(range(0, 2*pi, length=(number_of_spatial_cells + 2)))[2:end-1] # Adding 2 to make the function even at the transitions
-#x_0 = zeros(number_of_spatial_cells)
+#x_0 = 2*sin.(range(0, 2*pi, length=(number_of_spatial_cells + 2)))[2:end-1] # Adding 2 to make the function even at the transitions
+x_0 = zeros(number_of_spatial_cells)
 dx_0 = zeros(number_of_spatial_cells)
+
+# Defining constants for time property
+sim_time = (0.0, 1.1)
+Δt = 0.001
+t = sim_time[1]:Δt:sim_time[2]
+frequency = 50
+x = sin.(2*pi*frequency*t)
+
+# Exitation function
+f = LinearInterpolation(x, t)
+
 
 ## Analytical solution
 analytical_ddx_0 = -2*((2*pi)/100)^2*sin.(range(0, 2*pi, length=number_of_spatial_cells))
@@ -42,22 +48,23 @@ display(dx)
 display(Array(A_x))
 Q = Dirichlet0BC(Float64)
 
+function ddx(ddx, dx, x, p, t)
+    x[50] = x[50] + f(t)
+    ddx[:] = c*(A_x*Q)*x
+    #ddx[50] = f(t)
+end
 
-#ddx(ddu, du, u, p, t) = c*(A_x*Q)*u #- d*du # + f(t)
-
-function ddx(ddu, du, u, p, t)
-    ddu[:] = c*(A_x*Q)*u    #- d*du # + f(t)
+function exitate_x(func, exitation_functions, coords)
+    return (ddx, dx, x)
 end
 
 prob = SecondOrderODEProblem(ddx, dx_0, x_0, sim_time)
 
 ## Simulate
-sol = solve(prob, Rosenbrock23())
+sol = solve(prob, TRBDF2())
 
 ## Showing results
 
-du = plot(sol[1:100, end-10:end])
-u = plot(sol[101:end, end-10:end])
+du = plot(sol[1:100, :])
+u = plot(sol[101:end, :])
 display(plot(u, du, layout = (2,1), size=(1400,600)))
-
-
