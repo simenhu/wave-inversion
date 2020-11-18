24e87e67a382f344cfea2ccead22232721311eaf
Simen Husøy <simen@birdsview.no>
2020-11-18 23:45:03 +0100

    first working measure of energy in simulation. Should find method to verify the calculation

--- src/1D_error_testing_analytical.jl
+++ src/1D_error_testing_analytical.jl
@@ -57,7 +57,7 @@ u = plot(sol[101:end, 1],)
 display(plot(u, du, layout = (2,1), size=(1400,600)))
 
 ## plot image of state compared with analytical solution
-time_res = 0.01
+time_res = 0.005
 time_vector = sim_time[1]:time_res:sim_time[2]
 analytical_sol = [an_u(x,t) for x in internal_positions[2:end-1], t in time_vector]
 simulated_sol = sol(time_vector)[101:end,:]
@@ -65,5 +65,30 @@ analytical_plot = plot(Gray.(analytical_sol))
 state_plot = plot(Gray.(simulated_sol))
 error_plot = plot(Gray.(analytical_sol - simulated_sol))
 
-display(plot(analytical_plot, state_plot, error_plot, layout = (3, 1), size=(1400, 600)))
+display(plot(analytical_plot, state_plot, error_plot, layout = (3, 1), size=(1400, 600), link = :x))
 
+## Define energy of a state
+"""
+want to integrate over the spatial region of the string
+"""
+
+
+"""
+Function for calculating energy of a vector. Input should be state vector after simulating. This
+means that the first part of vector is the state time derivative, and second part is the state. 
+"""
+## Define function for energy
+function energy_of_1D_system(du, u,  T, μ)
+    A_x = CenteredDifference(2, 3, dx, number_of_spatial_cells)
+    Q = Dirichlet0BC(Float64)
+    energy = 1/2*T*dot(-(A_x*Q)*u,u) + μ*dot(du, du)
+end
+
+##
+errors = []
+for i in 0:0.01:sim_time[2]
+    e = energy_of_1D_system(sol(i).x[1], sol(i).x[2], T, μ)
+    push!(errors, e)
+end
+
+plot(errors)
