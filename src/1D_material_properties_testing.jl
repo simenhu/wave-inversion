2f05b553ace94a1bb83cc95ddda5d9bff6dbf2fe
Simen Husøy <simen@birdsview.no>
2020-12-07 14:54:32 +0100

    fixed energyfunction with varying material properties

--- src/1D_material_properties_testing.jl
+++ src/1D_material_properties_testing.jl
@@ -25,18 +25,16 @@ t_vector = sim_time[1]:Δt:sim_time[2]
 frequency = 50
 x_excitation = sin.(2*pi*frequency*t_vector)
 
-
 f_excitation = gaussian_excitation_function(100, 0.005, sim_time, 0.1, 0.017)
-
 internal_positions = internal_node_positions(0, string_length, number_of_spatial_cells)
 
 ## Initial conditions
 x_0 = zeros(number_of_spatial_cells)
 dx_0 = zeros(number_of_spatial_cells)
-# f = in_place_1D_string(string_length, number_of_spatial_cells, T, μ, [f_excitation], [50])
 c_squared = zeros(100)
 c_squared[1:70] .= T/μ
 c_squared[70:end] .= 2*T/μ
+
 
 f = in_place_1D_string_with_coefficients(string_length, number_of_spatial_cells, c_squared, [f_excitation], [50])
 prob = SecondOrderODEProblem(f, dx_0, x_0, sim_time)
@@ -49,7 +47,7 @@ solvers =  [Tsit5(), TRBDF2(), Rosenbrock23(), AutoTsit5(Rosenbrock23()), Midpoi
 solver = solvers[6]
 sol = @timeit to "simulation" solve(prob, solver)
 
-energy = energy_of_string(sol, string_length, number_of_spatial_cells, T, μ)
+energy = energy_of_string(sol, string_length, number_of_spatial_cells, c_squared, T)
 root_squared_error(x1, x2) = vec(sum(sqrt.((x1-x2).^2), dims=1))
 
 ## plot image of state compared with analytical solution
--- src/simutils/analysis_tools.jl
+++ src/simutils/analysis_tools.jl
@@ -8,16 +8,17 @@ using DataInterpolations
 Calculates the energy for a 1D-string solution for each time step of the solution and interpolates the
 solution to enable ploting at any time step withhin the time range 
 """
-function energy_of_string(sol, string_length, internal_cells, T, μ)
+function energy_of_string(sol, string_length, internal_cells, coeffs, T)
     dx = string_length/(internal_cells + 1)
     A_x = CenteredDifference(2, 3, dx, internal_cells)
     Q = Dirichlet0BC(Float64)
+    μ = T ./ coeffs
     
     energy_array = zeros(length(sol))
     for i in eachindex(sol)
         du = sol[i].x[1]
         u = sol[i].x[2]
-        e = 1/2*T*dot(-(A_x*Q)*u,u) + 1/2*μ*dot(du, du)
+        e = 1/2*T*dot(-(A_x*Q)*u,u) + 1/2*dot(du, μ .*  du) # Elementwize multiplication to include spatial dependent mass density
         energy_array[i] = e
     end
     return LinearInterpolation(energy_array, sol.t)
